d2b284c133aceea4d112bf61f0cb4841e7c7fa05
yanghanliang <1218866243@qq.com>
2019-02-21 17:54:56 +0800

    上传图片添加格式&&大小限制

--- process.js
+++ process.js
@@ -711,7 +711,7 @@ module.exports.addReply = (req, res, data) => {
 module.exports.pictureUpload = (req, res) => {
     var form = new formidable.IncomingForm()
     //设置文件上传存放地址
-    form.uploadDir = "./user_head_portrait"
+    form.uploadDir = ".//user_head_portrait"
     // 保留图片的扩展名
     form.keepExtensions = true
     //执行里面的回调函数的时候，表单已经全部接收完毕了。
@@ -719,17 +719,29 @@ module.exports.pictureUpload = (req, res) => {
         // console.log("files:",files)  //这里能获取到图片的信息
         // console.log("fields:",fields) //这里能获取到传的参数的信息，如上面的message参数，可以通过fields。message来得到 
         // console.log("path:", files.file.path) // 获取图片路径
-        const commentId = fields.commentId,
-              head_portrait_url = files.file.path.replace(/[\\]/g, '//')
-        
-        const sql = `UPDATE comment SET head_portrait_url='${head_portrait_url}' WHERE comment_id=${commentId} `
-
-        connect.query(sql, (error, result, fields) => {
-            if (error) throw error
+        if (files.file.size/1024/1024 > 1) {
             res.send({
-                status: 200,
-                head_portrait_url: head_portrait_url
+                status: 201,
+                msg: '上传头像图片大小不能超过 1MB!'
             })
-        })
+        } else if (files.file.type.indexOf('image') !== -1) {
+            const commentId = fields.commentId,
+            head_portrait_url = files.file.path.replace(/[\\]/g, '\/\/')
+        
+            const sql = `UPDATE comment SET head_portrait_url='${head_portrait_url}' WHERE comment_id=${commentId} `
+
+            connect.query(sql, (error, result, fields) => {
+                if (error) throw error
+                res.send({
+                    status: 200,
+                    head_portrait_url: head_portrait_url
+                })
+            })
+        } else {
+            res.send({
+                status: 201,
+                msg: '请提交图片格式的文件，如后缀名为： .gif、.png、.jpg'
+            })
+        }
     })
 }
